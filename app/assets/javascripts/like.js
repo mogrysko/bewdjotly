d2878e89b827a8e29b1804077ff8c3e9cb7ac268
Mike Ogrysko <michael.ogrysko@comcast.net>
2015-05-01 19:23:06 -0400

    likes with text

--- app/assets/javascripts/like.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  $('.like form').on('ajax:success', function(){
-    var $btn = $(this).find('.btn')
-    $btn.toggleClass('btn-default').toggleClass('btn-success');
-    $btn.val($btn.hasClass('btn-default') ? 'Unlike' : 'Like');
-  });
-});
