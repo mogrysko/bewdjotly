3ddc73f1c800e824c5a7897d18f713c3b975e3ed
Mike Ogrysko <michael.ogrysko@comcast.net>
2015-05-02 16:27:08 -0400

    added follow/unfollow button, relationship model

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -6,6 +6,9 @@ module SessionHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+  def current_user?(user)
+    user == current_user
+  end
   def jot_author(jot)
     if jot.user_id == current_user.try(:id)
       'You'
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   root to: 'session#show'
   post '/' => 'session#create'
   delete '/session' => 'session#destroy'
-  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :jots do
     member do
       post :like
       post :unlike
     end
   end
+  resources :relationships, only: [:create, :destroy]
 end
